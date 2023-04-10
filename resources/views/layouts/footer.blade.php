0c5f7b8dc51bdc6c994aeaff3f2c4160710fe453
GAMR\gamr2 <gamr21@outlook.es>
2023-04-09 19:20:31 -0500

    base

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <strong>Copyright &copy; {{date('Y')}} <a href="">SISTEMA JUDICIAL</a>.</strong>
+    Todos los derechos reservados.
+    <div class="float-right d-none d-sm-inline-block">
+    </div>
+  </footer>
